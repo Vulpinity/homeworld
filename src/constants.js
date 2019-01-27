9c1b3d44c9d1940fb827b7c252da3dac6794688a
Flaise <x.Flaise@gmail.com>
2019-01-27 12:18:40 -0600

    Tweaked acceleration and boundary pressure

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,5 @@ exports.MIN_LEN_LASER = 1;
 exports.BOUNDS_DIAMETER = 30;
 exports.TIME_SPAWN = 2000;
 exports.ACCELERATION_FACTOR = 4
-exports.MAX_ACCELERATION = 4
+exports.MAX_ACCELERATION = 3.5
 exports.FRICTION_FACTOR = 1
